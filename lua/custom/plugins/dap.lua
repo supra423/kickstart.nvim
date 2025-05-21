330b1e9fef24d51e2db9c46bc385a433ea395544
supra <alexusenzoroa@gmail.com>
2025-05-21 11:59:49 +0800

    Linux ubuntu DAP change

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -7,7 +7,7 @@ return {
       {
         'mfussenegger/nvim-dap-python',
         config = function()
-          require('dap-python').setup 'python'
+          require('dap-python').setup 'python3'
         end,
       },
     },
